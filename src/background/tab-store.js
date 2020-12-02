4e279d090140a19a65f7d124d8d1ea17bdeefc55
Eduardo Sanz García <esanzgar@gmail.com>
2020-12-02 11:03:34 +0100

    Remove support for old format in the localStorage
    5 years ago a more flexible, multi-key object was introduced to capture the relevant information about the tab. The old format, a `"inactive"|"active"` string was retired.
    
    On every extension update the code that runs the conversion is executed. By now, every localStorage is in the new format, therefore this legacy code can safely be retired.

--- src/background/tab-store.js
+++ src/background/tab-store.js
@@ -40,16 +40,7 @@ export default function TabStore(storage) {
     try {
       local = {};
       const loaded = JSON.parse(storage.getItem(key));
-      Object.keys(loaded).forEach(function (key) {
-        // ignore tab state saved by earlier versions of
-        // the extension which saved the state as a {key: <state string>}
-        // dict rather than {key: <state object>}
-        if (typeof loaded[key] === 'string') {
-          local[key] = { state: loaded[key] };
-        } else {
-          local[key] = loaded[key];
-        }
-      });
+      Object.keys(loaded).forEach(key => (local[key] = loaded[key]));
     } catch (e) {
       local = null;
     }
