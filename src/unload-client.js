e471cb27a31b021193c9bc2d83651ac55d93c39c
Robert Knight <robertknight@gmail.com>
2020-12-02 10:47:40 +0000

    Fix error when deactivating extension a second time in a tab
    Fix an error about a duplicate `annotatorLink` variable when
    de-activating the extension in a tab where it had previously been
    active.
    
    It appears that injected scripts all share the same JS global, so if the
    `unload-client.js` script is injected multiple times then subsequent
    runs would fail due to the `annotatorLink` variable having already been
    declared. This didn't happen in the past because the `const` was
    transpiled down to `var`, which allows redeclaration. This no longer
    happens.

--- src/unload-client.js
+++ src/unload-client.js
@@ -3,13 +3,19 @@
 
 'use strict';
 
-const annotatorLink = document.querySelector(
-  'link[type="application/annotator+html"]'
-);
+// This wrapper function ensures that variables declared within `unloadClient`
+// are not visible to any scripts that are subsequently run by the JS extension.
+function unloadClient() {
+  const annotatorLink = document.querySelector(
+    'link[type="application/annotator+html"]'
+  );
 
-if (annotatorLink) {
-  // Dispatch a 'destroy' event which is handled by the code in
-  // annotator/main.js to remove the client.
-  const destroyEvent = new Event('destroy');
-  annotatorLink.dispatchEvent(destroyEvent);
+  if (annotatorLink) {
+    // Dispatch a 'destroy' event which is handled by the code in
+    // annotator/main.js to remove the client.
+    const destroyEvent = new Event('destroy');
+    annotatorLink.dispatchEvent(destroyEvent);
+  }
 }
+
+unloadClient();
