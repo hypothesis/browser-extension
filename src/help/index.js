34a719880a13f907e5bde55b2564632d26069d2f
Robert Knight <robertknight@gmail.com>
2021-09-10 06:45:09 +0100

    Replace a query-string parsing utility with URLSearchParams

--- src/help/index.js
+++ src/help/index.js
@@ -1,19 +1,5 @@
 'use strict';
 
-/** Parse a query string into an object mapping param names to values. */
-function parseQuery(query) {
-  if (query.charAt(0) === '?') {
-    query = query.slice(1);
-  }
-  return query.split('&').reduce(function (map, entry) {
-    const keyValue = entry.split('=').map(function (e) {
-      return decodeURIComponent(e);
-    });
-    map[keyValue[0]] = keyValue[1];
-    return map;
-  }, {});
-}
-
 // Detect the current OS and show approprite help.
 chrome.runtime.getPlatformInfo(function (info) {
   const opts = /** @type {NodeListOf<HTMLElement>} */ (
@@ -26,10 +12,12 @@ chrome.runtime.getPlatformInfo(function (info) {
   });
 });
 
-const query = parseQuery(window.location.search);
-if (query.message) {
+const query = new URLSearchParams(window.location.search);
+const message = query.get('message');
+
+if (message) {
   const errorTextEl = /** @type {HTMLElement} */ (
     document.querySelector('.js-error-message')
   );
-  errorTextEl.textContent = query.message;
+  errorTextEl.textContent = message;
 }
