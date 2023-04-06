3df9fc0ba8f4f1c02f2a004a86390d81c458c282
Robert Knight <robertknight@gmail.com>
2023-04-06 12:51:03 +0100

    Enable messages from bouncer to activate the extension
    Add support for a new external message, "activate", which enables the extension
    on the tab that sent the message. The existing "ping" message also gains
    support for a `queryFeatures` field which allows bouncer to test whether
    the installed extension supports the "activate" message.
    
    This enables the bouncer service to activate a specific Hypothesis extension
    directly, as opposed to having all installed Hypothesis extensions watch and
    respond to the presence of an `#annotations:` fragment in the URL. The latter
    approach causes a conflict when multiple Hypothesis extensions are installed.
    
    Part of #1177

---
+++ src/background/install.ts
@@ -0,0 +1,66 @@
+import { chromeAPI } from './chrome-api';
+import { Extension } from './extension';
+import type { ExternalMessage } from './messages';
+
+export async function init() {
+  const extension = new Extension();
+  const initialized = extension.init();
+
+  // Tokens indicating which features the current extension supports.
+  const allFeatures = [
+    // "activate" message to activate extension on current tab and
+    // optionally first navigate to a different URL.
+    'activate',
+  ];
+
+  chromeAPI.runtime.onInstalled.addListener(async installDetails => {
+    // Check whether this is the inital installation or an update of an existing
+    // installation.
+    if (installDetails.reason === 'install') {
+      const extensionInfo = await chromeAPI.management.getSelf();
+      extension.firstRun(extensionInfo);
+    }
+  });
+
+  // Respond to messages sent by the JavaScript from https://hyp.is.
+  // This is how it knows whether the user has this Chrome extension installed.
+  chromeAPI.runtime.onMessageExternal.addListener(
+    (request: ExternalMessage, sender, sendResponse) => {
+      switch (request.type) {
+        case 'ping':
+          {
+            const queryFeatures = request.queryFeatures ?? [];
+            const features = allFeatures.filter(f => queryFeatures.includes(f));
+            sendResponse({ type: 'pong', features });
+          }
+          break;
+        case 'activate':
+          {
+            if (typeof sender.tab?.id !== 'number') {
+              return;
+            }
+
+            const { url, query } = request;
+            if (url) {
+              chromeAPI.tabs.update(sender.tab.id, { url });
+            }
+            extension.activate(sender.tab.id, {
+              afterNavigationTo: url,
+              query,
+            });
+
+            sendResponse({ active: true });
+          }
+          break;
+      }
+    }
+  );
+
+  chromeAPI.runtime.requestUpdateCheck?.().then(() => {
+    chromeAPI.runtime.onUpdateAvailable.addListener(() =>
+      chromeAPI.runtime.reload()
+    );
+  });
+
+  await initialized;
+}
