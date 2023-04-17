e7a6bbb256966a6e9a3ddb5382149e20e57de8cf
Robert Knight <robertknight@gmail.com>
2023-04-17 11:59:47 +0100

    Convert direct-link-query.js to TypeScript

--- src/background/direct-link-query.ts
+++ src/background/direct-link-query.ts
@@ -3,12 +3,15 @@
  * particular set of annotations automatically after it loads.
  *
  * See https://h-client.readthedocs.io/en/latest/publishers/config/#config-settings
- *
- * @typedef Query
- * @property {string} [annotations] - ID of the direct-linked annotation
- * @property {string} [query] - Filter query from the sidebar
- * @property {string} [group] - ID of the direct-linked group
  */
+export type Query = {
+  /** ID of the direct-linked annotation. */
+  annotations?: string;
+  /** Filter query for the sidebar. */
+  query?: string;
+  /** ID of the direct-linked group. */
+  group?: string;
+};
 
 /**
  * Extracts the direct-linking query from the URL if any.
@@ -16,13 +19,12 @@
  * If present, the query causes the extension to activate automatically and
  * show the matching set of annotations.
  *
- * @param {string} url -
+ * @param url -
  *   The URL which may contain a '#annotations:' fragment specifying which
  *   annotations to show.
- * @return {Query|null}
- *   The direct link query translated into client configuration settings.
+ * @return - The direct link query translated into client configuration settings.
  */
-export function directLinkQuery(url) {
+export function directLinkQuery(url: string): Query | null {
   // Annotation IDs are url-safe-base64 identifiers
   // See https://tools.ietf.org/html/rfc4648#page-7
   const idMatch = url.match(/#annotations:([A-Za-z0-9_-]+)$/);
