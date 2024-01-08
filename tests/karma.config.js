bfa879479e232a1c5ad3f9d1b84df6ebf38ca2bb
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-01-08 16:18:20 +0100

    Remove dependency on karma-sinon and karma-chai

--- tests/karma.config.js
+++ tests/karma.config.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon', 'source-map-support'],
+    frameworks: ['mocha', 'source-map-support'],
 
     // list of files / patterns to load in the browser
     files: [
