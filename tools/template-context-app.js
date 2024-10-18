2a0751896a52e2283eff42706bf1fecf1ac6e977
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-10-18 14:43:45 +0200

    Use TS shared config from eslint-config-hypothesis

--- tools/template-context-app.js
+++ tools/template-context-app.js
@@ -9,7 +9,7 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 function appSettings(settings) {
-  let result = {};
+  const result = {};
   result.apiUrl = settings.apiUrl;
   result.assetRoot = '/client/';
   result.authDomain = settings.authDomain;
